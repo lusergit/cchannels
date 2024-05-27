10dae200b55cb60f2c519a3e270aa36ad7837b78
Luca Zaninotto (lusergit) <lucazanny@gmail.com>
2024-05-27 12:48:40 +0200

    Repo init, first implementation

---
+++ src/channels.h
@@ -0,0 +1,71 @@
+#include "queue.h"
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+
+/* Channels and their operations, essentially making queue.h thread
+   safe */
+
+#ifndef CHANNELS_H
+#define CHANNELS_H
+
+#define CHANNEL(TYPE)							\
+  /* Init queues of that type */					\
+  QUEUE(TYPE);								\
+  typedef struct TYPE##_channel {						\
+    TYPE##_queue *messages;						\
+    pthread_mutex_t *lock;						\
+    sem_t *sem;								\
+  } TYPE##_channel;									\
+									\
+  void TYPE##_channel_init(TYPE##_channel *c) {				\
+    c->messages = (TYPE##_queue *)malloc(sizeof(TYPE##_queue));		\
+    c->lock = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));	\
+    c->sem = (sem_t *)malloc(sizeof(sem_t));				\
+    TYPE##_queue_init(c->messages);					\
+    pthread_mutex_init(c->lock, NULL);					\
+    sem_init(c->sem, 0, 0);						\
+  }									\
+									\
+  typedef struct TYPE##_send_args {					\
+    TYPE##_channel* c;							\
+    TYPE *value;							\
+  } TYPE##_send_args;							\
+  									\
+  void* TYPE##_psend(void *args) {					\
+    struct TYPE##_send_args* targs = args;				\
+    /* write on queue */						\
+    pthread_mutex_lock((targs->c)->lock);					\
+    TYPE##_push((targs->c)->messages, targs->value);			\
+    pthread_mutex_unlock((targs->c)->lock);				\
+    /* signal semaphore that a new message is in queue */		\
+    sem_post((targs->c)->sem);						\
+    return NULL;							\
+  }									\
+  									\
+  /* send a message on channel C. void since it guarantees that the	\
+     message will eventually be written in the queue */			\
+  void TYPE##_snd(TYPE##_channel* c, TYPE * v) {			\
+    struct TYPE##_send_args* args =					\
+      (struct TYPE##_send_args *)malloc(sizeof(struct TYPE##_send_args)); \
+    args->c = c;							\
+    args->value = v;							\
+    pthread_t thread;							\
+    /* Run primitive send in the same thread */				\
+    pthread_create(&thread, NULL, TYPE##_psend, (void *)&args);		\
+  }									\
+  									\
+  /* Recieve a message from channel C. Returns a pointer to the		\
+     message */								\
+  TYPE* TYPE##_rcv (TYPE##_channel* c) {				\
+    TYPE *value = NULL;							\
+    do {								\
+      sem_wait(c->sem);							\
+      pthread_mutex_lock(c->lock);					\
+      value = TYPE##_pop(c->messages);					\
+      pthread_mutex_unlock(c->lock);					\
+    } while (!value);							\
+    return value;							\
+  }									\
+
+#endif /* CHANNELS_H */
---
+++ src/queue.h
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Queue with its operations, call QUEUE(type) to build the queue
+   operations and structures for that type. */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#define QUEUE(TYPE)							\
+  /* Node type */							\
+  typedef struct TYPE##_node {						\
+    TYPE* data;								\
+    struct TYPE##_node *next;						\
+  } TYPE##_node;							\
+									\
+  /* Queue type */							\
+  typedef struct TYPE##_queue {							\
+    TYPE##_node *front;							\
+    TYPE##_node *rear;							\
+  } TYPE##_queue;									\
+									\
+  /* INIT queue */							\
+  void TYPE##_queue_init(TYPE##_queue *q) {				\
+    q->front = q->rear = NULL;						\
+  }									\
+									\
+  /* enqueue operation FIFO mode */					\
+  /* 0 if ok, 1 -> error */						\
+  int TYPE##_push(TYPE##_queue *q, TYPE* value) {			\
+    TYPE##_node *new_node = (TYPE##_node *)malloc(sizeof(TYPE##_node));	\
+    if (!new_node) {							\
+      fprintf(stderr, "Memory allocation error\n");			\
+      return 1;								\
+    }									\
+    new_node->data = value;						\
+    new_node->next = NULL;						\
+    if (q->rear == NULL) {						\
+      q->front = q->rear = new_node;					\
+    } else {								\
+      q->rear->next = new_node;						\
+      q->rear = new_node;						\
+    }									\
+    return 0;								\
+  }									\
+									\
+  /* Dequeue, also FIFO mode */						\
+  TYPE* TYPE##_pop(TYPE##_queue *q) {					\
+    if (q->front == NULL) {						\
+      /* No error message, just return null, callee responsability */	\
+      return NULL;							\
+    }									\
+    TYPE##_node *temp = q->front;					\
+    TYPE* value = temp->data;						\
+    q->front = q->front->next;						\
+    									\
+    if (q->front == NULL) {						\
+      q->rear = NULL;							\
+    }									\
+    free(temp);								\
+    return value;							\
+  }									\
+
+#endif /* QUEUE_H */
